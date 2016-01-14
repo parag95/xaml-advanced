9d78dd58ffc865734fcd7bcf27459ea5f84a6dac
parag95 <paragjain95@gmail.com>
2016-01-14 23:44:04 +0530

    Module-2 HW

--- RestaurantManager.Models/ExpediteDataManager.cs
+++ RestaurantManager.Models/ExpediteDataManager.cs
@@ -8,7 +8,14 @@ namespace RestaurantManager.Models
         {
 
         }
-
+        public ExpediteDataManager()
+        {
+            this._orderItems = new List<Order>
+            {
+                new Order { Items= new List<MenuItem> { new MenuItem() {Title="French Bread & Fondue Dip" } }, SpecialRequests="Extra" },
+                new Order { Items=new List<MenuItem> { new MenuItem() { Title = "Mashed Peas" } }, SpecialRequests="" }
+            };
+        }
         private List<Order> _orderItems;
         public List<Order> OrderItems
         {
